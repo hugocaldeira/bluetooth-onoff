983bc28838b6ac832f4b3d16e0a411656adbf009
Hugo Caldeira <hugo.caldeira@talkdesk.com>
2022-11-25 23:04:33 +0000

    remove comments from script file

--- bluetooth-onoff.sh
+++ bluetooth-onoff.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# brew install blueutil
-# */1 * * * * /Users/hugocaldeira/projects/script/bluetooth-onoff.sh
 
 a=`/usr/sbin/ioreg -r -k AppleClamshellState -d 4 | grep AppleClamshellState  | head -1 | grep 'No'`
 if [ -z "$a" ]; then 
